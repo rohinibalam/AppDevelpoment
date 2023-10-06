457e92f567e07cb17ea2eb64c5e0cad520279551
rohinibalam <104961774+rohinibalam@users.noreply.github.com>
2023-10-06 12:20:56 +0530

    Add files via upload

---
+++ Day 14/src/main/java/com/example/demo/modal/User.java
@@ -0,0 +1,153 @@
+package com.example.demo.modal;
+import java.time.LocalDateTime;
+//import java.util.ArrayList;
+//import java.util.List;
+
+import com.example.demo.user.domain.UserRole;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+
+//import jakarta.persistence.CascadeType;
+//import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+//import jakarta.persistence.ElementCollection;
+//import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "password")
+    private String password;
+    
+    /*@Column(name = "confirmpassword")
+    private String confirmpassword;*/
+
+
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "address")
+    private String address;
+    
+    
+    
+    private String mobile;
+
+    private LocalDateTime createdAt;
+    
+    public User() {
+		
+	}
+
+	public User(Long id, String firstName, String lastName, String password, String email, String mobile,
+			 LocalDateTime createdAt) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		//this.confirmpassword = confirmpassword;
+		this.email = email;
+		this.mobile = mobile;
+		this.address = address;
+		
+		this.createdAt = createdAt;
+	}
+
+	
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	/*public String getConfirmPassword() {
+		return confirmpassword;
+	}
+
+	public void setConfirmPassword(String confirmpassword) {
+		this.confirmpassword = confirmpassword;
+	}*/
+	
+
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	
+
+    
+}
