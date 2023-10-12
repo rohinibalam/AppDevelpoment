c7b8c9fd399922345da60deb4c38807892d8ab44
rohinibalam <104961774+rohinibalam@users.noreply.github.com>
2023-10-12 13:13:12 +0530

    Delete Day 15 directory

--- Day 15/src/main/java/com/example/demo/config/JwtConstant.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.config;
-
-public class JwtConstant {
-	
-	public static final String SECRET_KEY="kjhgtyedyesrdtfyhuiytiyuiyitu5ryguky7i6r5ydfhkgytrysdfh";
-	public static final String JWT_HEADER="Authorization";
-
-}
--- Day 15/src/main/java/com/example/demo/response/ServiceProviderResponse.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.response;
-
-public class ServiceProviderResponse {
-
-}
